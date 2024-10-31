9a953054a03e41608a2f28d5d136b59e5f036189
velinamons <velinamons@gmail.com>
2024-10-31 22:09:48 +0200

    Create django app 'game_catalog'; Add 'rest_framework' and 'game_catalog' to INSTALLED_APPS

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "game_catalog",
 ]
 
 MIDDLEWARE = [
@@ -49,13 +51,12 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "GameCatalogDRF.urls"
+ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
-        ,
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
