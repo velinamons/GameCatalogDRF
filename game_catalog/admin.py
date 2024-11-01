24f8e3ddcc42e85202984fab835670654bd44308
velinamons <velinamons@gmail.com>
2024-11-01 10:30:14 +0200

    Add models Genre, Studio, Game; Register models in admin.py

--- game_catalog/admin.py
+++ game_catalog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Game, Genre, Studio
 
-# Register your models here.
+admin.site.register(Game)
+admin.site.register(Genre)
+admin.site.register(Studio)
--- game_catalog/models.py
+++ game_catalog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Studio(models.Model):
+    name = models.CharField(max_length=100)
+    founded_year = models.IntegerField()
+    description = models.TextField()
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    release_year = models.IntegerField()
+    genre = models.ManyToManyField(Genre)
+    studio = models.ForeignKey(Studio, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
