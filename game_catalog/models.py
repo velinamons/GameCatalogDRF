813d54228e8d118ffb28d6a4a3a16f701cea102c
velinamons <velinamons@gmail.com>
2024-11-23 01:01:54 +0200

    Refactor models relations, add CustomUser model and set as AUTH_USER_MODEL; Add JWTAuthentication; Add RegisterSerializer, GameCommentSerializer, CommentSerializer, UserAdminSerializer and UserPublicSerializer

--- game_catalog/models.py
+++ game_catalog/models.py
@@ -1,16 +1,18 @@
-from django.contrib.auth.models import User
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils.timezone import now
 
 
-def validate_positive_year(value):
-    if value < 1950 or value > now().year:
-        raise ValidationError("Year must be between 0 and the current year.")
+def validate_year(value):
+    current_year = now().year
+    if value < 1950 or value > current_year:
+        raise ValidationError(f"Year must be between 1950 and {current_year}.")
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
 
     def __str__(self):
@@ -19,7 +21,7 @@ class Genre(models.Model):
 
 class Studio(models.Model):
     name = models.CharField(max_length=100)
-    founded_year = models.IntegerField(validators=[validate_positive_year])
+    founded_year = models.IntegerField(validators=[validate_year])
     description = models.TextField()
     country = models.CharField(max_length=100)
 
@@ -30,27 +32,23 @@ class Studio(models.Model):
 class Game(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    release_year = models.IntegerField(validators=[validate_positive_year])
+    release_year = models.IntegerField(validators=[validate_year])
     genre = models.ManyToManyField(Genre)
     studio = models.ForeignKey(Studio, on_delete=models.CASCADE)
-    comments = models.ManyToManyField('Comment', blank=True, related_name="games")
 
     def __str__(self):
         return self.name
 
 
+class CustomUser(AbstractUser):
+    favorite_games = models.ManyToManyField(Game, blank=True, related_name="favorited_by")
+
+
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments")
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name="comments")
     text = models.TextField()
     post_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Comment by {self.user.username} on {self.post_date}"
-
-
-class Favorite(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="favorites")
-    games = models.ManyToManyField(Game, blank=True)
-
-    def __str__(self):
-        return f"{self.user.username}'s Favorites"
+        return f"Comment by {self.user.username} on {self.post_date}: {self.text[:50]}"
