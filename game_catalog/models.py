2aa05ccbbf8786a77f5f18a9ac59b45c9e119907
velinamons <velinamons@gmail.com>
2024-11-24 17:54:22 +0200

    Refactor IntegerField 'release_year','founded_year' into DateField 'release_date', 'founded_date'; Remove year validation

--- game_catalog/models.py
+++ game_catalog/models.py
@@ -5,12 +5,6 @@ from django.core.exceptions import ValidationError
 from django.utils.timezone import now
 
 
-def validate_year(value):
-    current_year = now().year
-    if value < 1950 or value > current_year:
-        raise ValidationError(f"Year must be between 1950 and {current_year}.")
-
-
 class Genre(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
@@ -21,7 +15,7 @@ class Genre(models.Model):
 
 class Studio(models.Model):
     name = models.CharField(max_length=100)
-    founded_year = models.IntegerField(validators=[validate_year])
+    founded_date = models.DateField()
     description = models.TextField()
     country = models.CharField(max_length=100)
 
@@ -32,7 +26,7 @@ class Studio(models.Model):
 class Game(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    release_year = models.IntegerField(validators=[validate_year])
+    release_date = models.DateField()
     genre = models.ManyToManyField(Genre)
     studio = models.ForeignKey(Studio, on_delete=models.CASCADE)
 
