adc16cdd8bc4350297bc132a4768c859f2067739
velinamons <velinamons@gmail.com>
2024-11-25 22:09:41 +0200

    Remove redundant redirect url

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from game_catalog.views import RegisterView
 
 urlpatterns = [
-    path("", RedirectView.as_view(url="/register/", permanent=False)),
     path("register/", RegisterView.as_view(), name="register"),
     path("admin/", admin.site.urls),
     path("api/", include("game_catalog.urls")),
